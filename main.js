e61bb8e5e36b40319dc725dcda7e53cbb3e26844
Samu <samu17gr@gmail.com>
2024-09-03 15:02:00 +0200

    correccion de animaciones

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.getElementById('about-toggle').addEventListener('click', function() {
-  var content = document.getElementById('about-content');
+  let content = document.getElementById('about-content');
   content.classList.toggle('open');
   this.textContent = content.classList.contains('open') ? 'Ver menos' : 'Ver más';
 });
